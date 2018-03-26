f9587bf7c5233f622e855645414fafd86d60ce55
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-03-26 07:45:59 -0400

    edits

--- Chapter 8/_Ch8.ps1
+++ Chapter 8/_Ch8.ps1
@@ -9,3 +9,10 @@ Get-Process | Select-Object -Property Name,ID,VM,PM | `
 ConvertTo-Html | Out-File test2.html
 
 Get-Process | Select Name,ID,VM,PM | ConvertTo-Html | Out-File test3.html
+
+Get-Process | Sort-Object VM -descending | Select-Object Name,ID,VM
+
+Get-Process | Sort VM -descending | gm 
+Get-Process | Sort VM -descending | Select Name,ID,VM | gm
+
+Get-Process | Gm | Gm

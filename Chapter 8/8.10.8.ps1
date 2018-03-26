6e7c7026b6dbf6e8ba5386bf941a10e2022181f6
Josh Rawlins <joshua.rawlins@universalorlando.com>
2018-03-26 07:44:58 -0400

    lab

--- Chapter 8/8.10.8.ps1
+++ Chapter 8/8.10.8.ps1
@@ -2,7 +2,7 @@ Help *log*
 
 Help Get-EventLog
 
-Get-EventLog -LogName Security -Newest 50 | Sort-Object TimeGenerated,Index | `
-Select-Object Index,TimeGenerated,Source | Out-File securitylog.txt
+Get-EventLog -LogName Application -Newest 50 | Sort-Object TimeGenerated,Index | `
+Select-Object Index,TimeGenerated,Source | Out-File applog.txt
 
 Get-EventLog | gm
